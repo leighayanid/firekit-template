23655a129f0fd38a82212b2483eb1c92ff6078b5
Leigh Dinaya <leighdinaya@protonmail.com>
2022-01-17 15:38:18 +0800

    chore: remove unuse config files

--- src/config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDaCLgJ93nv4fFutqxZ64yR-84MkCQCUm4',
-  authDomain: 'vue3-firebase-auth-11e37.firebaseapp.com',
-  projectId: 'vue3-firebase-auth-11e37',
-  storageBucket: 'vue3-firebase-auth-11e37.appspot.com',
-  messagingSenderId: '787150429787',
-  appId: '1:787150429787:web:71d89efc12e5243794a97e'
-}
--- src/config/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from 'firebase/app'
-import 'firebase/firestore'
-import firebaseConfig from './firebaseConfig'
-const firebaseApp = initializeApp(firebaseConfig)
-
-// export firebaseapp
-export const firebase = firebaseApp.firestore()
