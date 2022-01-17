327484f86fd6f360c94a9ea8f3b512e562919205
Leigh Dinaya <leighdinaya@protonmail.com>
2022-01-18 07:45:43 +0800

    chore: prettify

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,19 @@ import CardSkeleton from '@/components/skeleton/CardSkeleton.vue'
 </script>
 
 <template>
-	<div class="md:px-80 md:py-10 p-4 dark:text-white text-slate-800">
-		<Navbar />
-		<!-- <router-view /> -->
-		<router-view v-slot="{ Component }" class="py-20">
-			<template v-if="Component">
-				<transition mode="out-in">
-					<keep-alive>
-						<component :is="Component"></component>
-					</keep-alive>
-				</transition>
-			</template>
-		</router-view>
-	</div>
+  <div class="md:px-80 md:py-10 p-4 dark:text-white text-slate-800">
+    <Navbar />
+    <!-- <router-view /> -->
+    <router-view v-slot="{ Component }" class="py-20">
+      <template v-if="Component">
+        <transition mode="out-in">
+          <keep-alive>
+            <component :is="Component"></component>
+          </keep-alive>
+        </transition>
+      </template>
+    </router-view>
+  </div>
 </template>
 
 <style></style>
--- src/components/skeleton/CardSkeleton.vue
+++ src/components/skeleton/CardSkeleton.vue
@@ -1,27 +1,27 @@
 <template>
-	<div class="bg-slate-800 p-5 rounded flex flex-col mb-2">
-		<SkeletonBlock
-			width="80%"
-			height="20px"
-			effect="wave"
-			border-radius="3px"
-			class="mb-2"
-		/>
+  <div class="bg-slate-800 p-5 rounded flex flex-col mb-2">
+    <SkeletonBlock
+      width="80%"
+      height="20px"
+      effect="wave"
+      border-radius="3px"
+      class="mb-2"
+    />
 
-		<skeleton-text tag="div" effect="wave" border-radius="3px">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet
-			ante dui. Praesent ut aliquam metus. Phasellus consectetur.
-		</skeleton-text>
-	</div>
+    <skeleton-text tag="div" effect="wave" border-radius="3px">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet
+      ante dui. Praesent ut aliquam metus. Phasellus consectetur.
+    </skeleton-text>
+  </div>
 </template>
 
 <script>
 import { SkeletonBlock, SkeletonText } from 'skeleton-elements/vue'
 
 export default {
-	components: {
-		SkeletonBlock,
-		SkeletonText
-	}
+  components: {
+    SkeletonBlock,
+    SkeletonText
+  }
 }
 </script>
